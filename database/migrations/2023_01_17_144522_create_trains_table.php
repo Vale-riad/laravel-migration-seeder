034c4ecc6379fa59b85c01bbaeb2072e6f311c17
Vale-riad <valentinariad91@gmail.com>
2023-01-17 17:18:29 +0100

    first commit

---
+++ database/migrations/2023_01_17_144522_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time', $precision = 0);
+            $table->time('arrival_time', $precision = 0);
+            $table->string('train_code', 100);
+            $table->unsignedTinyInteger('carriages');
+            $table->boolean('on_time')->default(1);
+            $table->boolean('canceled')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
